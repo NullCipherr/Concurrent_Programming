8cf6e64a2c508e0bd3d26133ca1a19324c6f6538
NullCipherr <blendsteal@gmail.com>
2024-07-31 20:07:09 -0300

    update code MPI section

--- MPI/MPI.c
+++ MPI/MPI.c
@@ -1,13 +1,16 @@
 /*
- * Compilar utilizando OpenMPI
- * mpicc arquivo -o arquivoMPI
+ * 1. Compilar utilizando OpenMPI
+ * 
+ *      mpicc arquivo -o arquivoMPI
  *
  * Este comando é utilizado para compilar um programa escrito em C que utiliza a biblioteca MPI (Message Passing Interface).
- * O comando 'mpicc' é o compilador específico para programas MPI. O parâmetro 'arquivo' é o nome do arquivo fonte que você deseja compilar,
+ * O comando 'mpicc' é o compilador específico para programas MPI. 
+ * O parâmetro 'arquivo' é o nome do arquivo fonte que você deseja compilar,
  * e '-o arquivoMPI' especifica o nome do arquivo executável que será gerado após a compilação.
  *
- * Executar utilizando OpenMPI
- * mpirun -np <n_processos> executavel <args>
+ * 2. Executar utilizando OpenMPI
+ *
+ *      mpirun -np <n_processos> executavel <args>
  *
  * Este comando é utilizado para executar um programa MPI. O comando 'mpirun' é o lançador de programas MPI.
  * O parâmetro '-np <n_processos>' especifica o número de processos que serão utilizados para executar o programa.
@@ -23,7 +26,24 @@
  * O parâmetro 'executavel' é o nome do arquivo executável que você deseja executar,
  * e '<args>' são os argumentos que você deseja passar para o programa.
  *
- * Configurar em hostfile (Ver depois)
+ * Configurar em hostfile
+ *
+ * Para configurar a execução em diversos hosts utilizando um arquivo de hostfile, siga os passos abaixo:
+ *
+ * 1. Crie um arquivo de texto chamado 'hostfile' com a lista de hosts onde o programa será executado.
+ *    Cada linha do arquivo deve conter o endereço IP ou o nome do host. Por exemplo:
+ *
+ *    hostfile:
+ *    192.168.1.1
+ *    192.168.1.2
+ *    192.168.1.3
+ *
+ * 2. Utilize o comando 'mpirun' com a opção '-hostfile' para especificar o arquivo de hostfile:
+ *
+ *    mpirun -hostfile hostfile -np <n_processos> executavel <args>
+ *
+ *    Este comando executará o programa MPI nos hosts especificados no arquivo 'hostfile',
+ *    utilizando o número de processos especificado por '-np <n_processos>'.
  *
  * -oversubscribe: Permite disparar um número maior de processos do que o número de núcleos do processador.
  *
@@ -32,6 +52,7 @@
  * mesmo que isso possa levar a uma sobrecarga do sistema.
  *
  * Rotinas Básicas:
+ *
  * Estas são algumas das rotinas básicas da biblioteca MPI:
  *
  * - MPI_Init: Inicializa o ambiente MPI. Deve ser chamada no início do programa.
---
+++ MPI/MPI_002/MPI_002.c
@@ -0,0 +1,85 @@
+// Aluno e RA
+//
+// Andrei Roberto da Costa RA: 107975
+
+/*
+
+Exemplo 02: Testes com a identificacao de processos no OPENMPI.
+
+Salve este arquivo com o nome exemplo02.c
+
+Compile-o em um terminal pela linha de comando, da seguinte forma:
+
+>mpicc exemplo02.c -o exemplo02 <enter>
+
+Depois execute-o pela linha de comando, da seguinte forma:
+
+>mpirun -np 5 ./exemplo02 bla ble bli blo blu <enter>
+
+Atencao: talvez seja necessario utilizar a opcao --oversubscribe na
+linha de comando do mpirun para permitir executar mais processos doque
+o numero de cores/processadores.
+
+Analise o codigo e a execucao. Teste diferentes numeros de processos.
+
+*/
+
+#include <stdio.h>
+#include <mpi.h>
+
+#define SIZE_VECTOR 1000
+int vetor[SIZE_VECTOR];
+
+void initialize_vector()
+{
+    for (int i = 0; i < SIZE_VECTOR; i++)
+    {
+        vetor[i] = i + 1;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int id, np, i;
+    int elements_por_processo;
+    int inicio_indice, final_indice;
+    int soma_local = 0;
+    double media_aritmetica = 0.0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+
+    elements_por_processo = SIZE_VECTOR / np;
+
+    initialize_vector();
+
+    printf("\nProcesso %d Quantidade = %d Argc = %d\n", id, np, argc);
+
+    /*
+        0 1 2 3 .. 500
+        500 501 502 .. 999
+    */
+
+    inicio_indice = id * elements_por_processo;
+    final_indice = (id == np - 1) ? SIZE_VECTOR : inicio_indice + elements_por_processo;
+
+    for (i = inicio_indice; i < final_indice; i++)
+    {
+        soma_local += vetor[i];
+    }
+
+    media_aritmetica = (double)soma_local / (final_indice - inicio_indice);
+
+    printf("Processo %d: Intervalo [%d, %d], Média = %.2f\n", id, inicio_indice, final_indice - 1, media_aritmetica);
+
+    MPI_Finalize();
+    return 0;
+}
+
+/* Desafio: Faca um programa que declare e inicialize estaticamente um
+vetor de 1000 elementos e divida esse vetor equilibradamente entre os
+processos, de forma que cada processo ira calcular a media aritmetica
+de sua parte. Cada processo deve imprimir na tela a sua identificacao
+(id ou rank), o seu intervalo dos dados, a sua parte do vetor e a media
+final */
